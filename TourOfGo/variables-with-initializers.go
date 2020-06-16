81dc65a2500001cf0f9de9a837c27da6c3e39ce2
coderhh <yehanghan@gmail.com>
2020-06-16 08:02:18 +0800

    add variables with initializers

---
+++ TourOfGo/variables-with-initializers.go
@@ -0,0 +1,18 @@
+/*
+变量的初始化
+
+变量的声明可以包含初始值，每个变量对应一个。
+如果初始值已经存在， 则可以省略类型；变量会从初始值中获得类型。
+*/
+
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func  main()  {
+	var c, python, java = true, false, "no!"
+
+	fmt.Println(i,j,c,python,java)
+}
