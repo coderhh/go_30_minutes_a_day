51543373cf9286ccf7aefe7d8b2237857a44f12a
coderhh <yehanghan@gmail.com>
2020-07-11 11:07:37 +0800

    verify value receiver and pointer receiver

--- GoByExample/Methods/methods.go
+++ GoByExample/Methods/methods.go
@@ -3,25 +3,26 @@ package main
 import "fmt"
 
 type rect struct {
-	width, height int
+	width, height, area, perim int
 }
 
-func (r *rect) area() int {
-	return r.width * r.height
+func (r *rect) areaFun() {
+	r.area = r.width * r.height
 }
 
-func (r rect) perim() int {
-	return 2*r.width + 2*r.height
+func (r rect) perimFun() {
+	r.perim = 2*r.width + 2*r.height
 }
 
 func main() {
-	r := rect{width: 10, height: 5}
-
-	fmt.Println("area: ", r.area())
-	fmt.Println("perim: ", r.perim())
+	r := rect{width: 10, height: 5, area: 0, perim: 0}
+	r.areaFun()
+	r.perimFun()
+	fmt.Println("area: ", r.area)
+	fmt.Println("perim: ", r.perim)
 
 	rp := &r
 
-	fmt.Println("area: ", rp.area())
-	fmt.Println("perim: ", rp.perim())
+	fmt.Println("area: ", rp.area)
+	fmt.Println("perim: ", rp.perim)
 }
