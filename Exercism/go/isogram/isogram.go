b057da6ba22c21978fc77ece4d0fb80d09fdc742
coderhh <yehanghan@gmail.com>
2020-08-18 08:53:08 +0800

    add isogram version two

--- Exercism/go/isogram/isogram.go
+++ Exercism/go/isogram/isogram.go
@@ -1,36 +1,20 @@
+// Package isogram detects isograms
 package isogram
 
 import (
-	"regexp"
 	"strings"
+	"unicode"
 )
 
-// IsIsogram return true if the input is an isogram
-func IsIsogram(input string) bool {
+// IsIsogram returns true if the input s is an isogram
+func IsIsogram(s string) bool {
+	s = strings.ToLower(s)
 
-	s := make([]string, 0)
-
-	r, _ := regexp.Compile("[^a-zA-Z]")
-	input = r.ReplaceAllString(input, "")
-	input = strings.ToUpper(input)
-	for i := range input {
-		_, found := Find(s, string(input[i]))
-		if found {
+	for i, c := range s {
+		if unicode.IsLetter(c) && strings.ContainsRune(s[i+1:], c) {
 			return false
 		}
-		s = append(s, string(input[i]))
 	}
 
 	return true
-}
-
-// Find takes a slice and looks for an element in it. If found it will
-// return it's key, otherwise it will return -1 and a bool of false.
-func Find(slice []string, val string) (int, bool) {
-	for i, item := range slice {
-		if item == val {
-			return i, true
-		}
-	}
-	return -1, false
 }
